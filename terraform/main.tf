b56933eedfb785d74dbe6a614be9510f1c5a4015
lukemartin35 <lukemartin35work@gmail.com>
2025-07-11 16:19:37 +0800

    update with cloud init

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,54 @@ resource "linode_instance" "mean_stack_vm" {
     "web-server",
     "ubuntu"
   ]
+
+# --- Cloud-Init Configuration ---
+  user_data = <<-EOF
+    #cloud-config
+    # Cloud-init directives start with #cloud-config
+
+    # Update and upgrade packages
+    apt:
+      update: true
+      upgrade: true
+
+    # Create a new user
+    users:
+      - name: {{ app_user }} # Using a variable from Terraform (see below)
+        groups: sudo # Add to sudo group for administrative tasks
+        shell: /bin/bash
+        sudo: ALL=(ALL) NOPASSWD:ALL # Grant passwordless sudo for automation
+        ssh_authorized_keys:
+          - {{ ssh_public_key }} # Add the same public key for this user
+
+    # Run arbitrary commands (e.g., install basic tools)
+    runcmd:
+      - echo "Cloud-init script started." >> /var/log/cloud-init-output.log
+      - apt-get update -y
+      - apt-get install -y git curl wget # Install common tools
+      - echo "Basic tools installed." >> /var/log/cloud-init-output.log
+
+    # Ensure the app directory exists and has correct permissions
+    # This is often better handled by Ansible later, but shown for completeness
+    # - mkdir -p /var/www/mean-app
+    # - chown {{ app_user }}:{{ app_user }} /var/www/mean-app
+
+    # You can add more complex shell scripts here
+    # For instance, to install Node.js using NodeSource PPA, though Ansible is better for this.
+    # - curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+    # - apt-get install -y nodejs
+
+    # You can also use write_files to create files
+    # write_files:
+    #   - path: /etc/my-custom-config.conf
+    #     permissions: '0644'
+    #     content: |
+    #       MyCustomSetting=true
+    #       AnotherSetting=value
+
+  EOF
 }
+
 
 # Output the IP address of the newly created VM
 # THIS IS THE CORRECT PLACEMENT FOR THE OUTPUT BLOCK
@@ -45,3 +92,7 @@ output "ip_address" {
   value       = linode_instance.mean_stack_vm.ip_address
   description = "The public IPv4 address of the Linode VM."
 }
+
+  
+# Add these variables to your variables.tf if you haven't already
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,15 @@ variable "ssh_public_key" {
   type        = string
   sensitive   = false
 }
+
+variable "app_user" {
+  description = "The username for the application."
+  type        = string
+  default     = "meanappuser" # Or make it a variable you pass from GHA
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key to add to the instance."
+  type        = string
+  sensitive   = true # Mark as sensitive
+}
